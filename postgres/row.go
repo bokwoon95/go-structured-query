4c6f09e6c87e5284cf2dfafba93b7f78d552b122
bokwoon <bokwoon.c@gmail.com>
2020-11-27 20:31:36 +0800

    Unexported irrelevant functions
    the Row* family of functions already exist as methods on the Row struct,
    I'm not sure why I added them in the first place. In addition they would
    be outdated by the time Go Generics arrive since a single generic function
    would be able to take their place. So I'm unexporting them in
    preparation for that.

--- postgres/row.go
+++ postgres/row.go
@@ -177,32 +177,32 @@ func (r *Row) NullBool(predicate Predicate) sql.NullBool {
 
 // Float64 returns the float64 value of the NumberField.
 func (r *Row) Float64(field NumberField) float64 {
-	return RowNullFloat64(r, field).Float64
+	return rowNullFloat64(r, field).Float64
 }
 
 // Float64Valid returns the bool value indicating if the NumberField is
 // non-NULL.
 func (r *Row) Float64Valid(field NumberField) bool {
-	return RowNullFloat64(r, field).Valid
+	return rowNullFloat64(r, field).Valid
 }
 
 // NullFloat64 returns the sql.NullFloat64 value of the NumberField.
 func (r *Row) NullFloat64(field NumberField) sql.NullFloat64 {
-	return RowNullFloat64(r, field)
+	return rowNullFloat64(r, field)
 }
 
-// RowFloat64 returns the float64 value of a Field
-func RowFloat64(r *Row, field Field) float64 {
-	return RowNullFloat64(r, field).Float64
+// rowFloat64 returns the float64 value of a Field
+func rowFloat64(r *Row, field Field) float64 {
+	return rowNullFloat64(r, field).Float64
 }
 
-// RowFloat64Valid returns the bool value indicating if the Field is non-NULL.
-func RowFloat64Valid(r *Row, field Field) bool {
-	return RowNullFloat64(r, field).Valid
+// rowFloat64Valid returns the bool value indicating if the Field is non-NULL.
+func rowFloat64Valid(r *Row, field Field) bool {
+	return rowNullFloat64(r, field).Valid
 }
 
-// RowNullFloat64 returns the sql.NullFloat64 value of the Field.
-func RowNullFloat64(r *Row, field Field) sql.NullFloat64 {
+// rowNullFloat64 returns the sql.NullFloat64 value of the Field.
+func rowNullFloat64(r *Row, field Field) sql.NullFloat64 {
 	if r.rows == nil {
 		r.fields = append(r.fields, field)
 		r.dest = append(r.dest, &sql.NullFloat64{})
@@ -217,29 +217,29 @@ func RowNullFloat64(r *Row, field Field) sql.NullFloat64 {
 
 // Int returns the int value of the NumberField.
 func (r *Row) Int(field NumberField) int {
-	return int(RowNullInt64(r, field).Int64)
+	return int(rowNullInt64(r, field).Int64)
 }
 
 // IntValid returns the bool value indicating if the NumberField is non-NULL.
 func (r *Row) IntValid(field NumberField) bool {
-	return RowNullInt64(r, field).Valid
+	return rowNullInt64(r, field).Valid
 }
 
-// RowInt returns the int value of the Field.
-func RowInt(r *Row, field Field) int {
-	return int(RowNullInt64(r, field).Int64)
+// rowInt returns the int value of the Field.
+func rowInt(r *Row, field Field) int {
+	return int(rowNullInt64(r, field).Int64)
 }
 
-// RowIntValid returns the bool value indicating if the Field is non-NULL.
-func RowIntValid(r *Row, field Field) bool {
-	return RowNullInt64(r, field).Valid
+// rowIntValid returns the bool value indicating if the Field is non-NULL.
+func rowIntValid(r *Row, field Field) bool {
+	return rowNullInt64(r, field).Valid
 }
 
 /* int64 */
 
 // Int64 returns the int64 value of the NumberField.
 func (r *Row) Int64(field NumberField) int64 {
-	return RowNullInt64(r, field).Int64
+	return rowNullInt64(r, field).Int64
 }
 
 // Int64Valid returns the bool value indicating if the NumberField is non-NULL.
@@ -249,21 +249,21 @@ func (r *Row) Int64Valid(field NumberField) bool {
 
 // NullInt64 returns the sql.NullInt64 value of the NumberField.
 func (r *Row) NullInt64(field NumberField) sql.NullInt64 {
-	return RowNullInt64(r, field)
+	return rowNullInt64(r, field)
 }
 
-// RowInt64 returns the int64 value of the Field.
-func RowInt64(r *Row, field NumberField) int64 {
-	return RowNullInt64(r, field).Int64
+// rowInt64 returns the int64 value of the Field.
+func rowInt64(r *Row, field NumberField) int64 {
+	return rowNullInt64(r, field).Int64
 }
 
-// RowInt64Valid returns the bool value indicating if the Field is non-NULL.
-func RowInt64Valid(r *Row, field NumberField) bool {
-	return RowNullInt64(r, field).Valid
+// rowInt64Valid returns the bool value indicating if the Field is non-NULL.
+func rowInt64Valid(r *Row, field NumberField) bool {
+	return rowNullInt64(r, field).Valid
 }
 
-// RowNullInt64 returns the sql.NullInt64 value of the Field.
-func RowNullInt64(r *Row, field Field) sql.NullInt64 {
+// rowNullInt64 returns the sql.NullInt64 value of the Field.
+func rowNullInt64(r *Row, field Field) sql.NullInt64 {
 	if r.rows == nil {
 		r.fields = append(r.fields, field)
 		r.dest = append(r.dest, &sql.NullInt64{})
@@ -278,32 +278,32 @@ func RowNullInt64(r *Row, field Field) sql.NullInt64 {
 
 // String returns the string value of the StringField.
 func (r *Row) String(field StringField) string {
-	return RowNullString(r, field).String
+	return rowNullString(r, field).String
 }
 
 // StringValid returns the bool value indicating if the StringField is
 // non-NULL.
 func (r *Row) StringValid(field StringField) bool {
-	return RowNullString(r, field).Valid
+	return rowNullString(r, field).Valid
 }
 
 // NullString returns the sql.NullString value of the StringField.
 func (r *Row) NullString(field StringField) sql.NullString {
-	return RowNullString(r, field)
+	return rowNullString(r, field)
 }
 
-// RowString returns the string value of the Field.
-func RowString(r *Row, field Field) string {
-	return RowNullString(r, field).String
+// rowString returns the string value of the Field.
+func rowString(r *Row, field Field) string {
+	return rowNullString(r, field).String
 }
 
-// RowStringValid returns the bool value indicating if the Field is non-NULL.
-func RowStringValid(r *Row, field Field) bool {
-	return RowNullString(r, field).Valid
+// rowStringValid returns the bool value indicating if the Field is non-NULL.
+func rowStringValid(r *Row, field Field) bool {
+	return rowNullString(r, field).Valid
 }
 
-// RowNullString returns the sql.NullString value of the Field.
-func RowNullString(r *Row, field Field) sql.NullString {
+// rowNullString returns the sql.NullString value of the Field.
+func rowNullString(r *Row, field Field) sql.NullString {
 	if r.rows == nil {
 		r.fields = append(r.fields, field)
 		r.dest = append(r.dest, &sql.NullString{})
@@ -318,31 +318,31 @@ func RowNullString(r *Row, field Field) sql.NullString {
 
 // Time returns the time.Time value of the TimeField.
 func (r *Row) Time(field TimeField) time.Time {
-	return RowNullTime(r, field).Time
+	return rowNullTime(r, field).Time
 }
 
 // TimeValid returns a bool value indicating if the TimeField is non-NULL.
 func (r *Row) TimeValid(field TimeField) bool {
-	return RowNullTime(r, field).Valid
+	return rowNullTime(r, field).Valid
 }
 
 // NullTime returns the sql.NullTime value of the TimeField.
 func (r *Row) NullTime(field TimeField) sql.NullTime {
-	return RowNullTime(r, field)
+	return rowNullTime(r, field)
 }
 
-// RowTime returns the time.Time value of the Field.
-func RowTime(r *Row, field Field) time.Time {
-	return RowNullTime(r, field).Time
+// rowTime returns the time.Time value of the Field.
+func rowTime(r *Row, field Field) time.Time {
+	return rowNullTime(r, field).Time
 }
 
-// RowTimeValid returns a bool value indicating if the Field is non-NULL.
-func RowTimeValid(r *Row, field Field) bool {
-	return RowNullTime(r, field).Valid
+// rowTimeValid returns a bool value indicating if the Field is non-NULL.
+func rowTimeValid(r *Row, field Field) bool {
+	return rowNullTime(r, field).Valid
 }
 
-// RowNullTime returns the sql.NullTime value of the Field.
-func RowNullTime(r *Row, field Field) sql.NullTime {
+// rowNullTime returns the sql.NullTime value of the Field.
+func rowNullTime(r *Row, field Field) sql.NullTime {
 	if r.rows == nil {
 		r.fields = append(r.fields, field)
 		r.dest = append(r.dest, &sql.NullTime{})
