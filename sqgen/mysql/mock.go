798fae24dd26c798594963a1c06a7f482be83bd2
Quentin Fox <quentinbennettfox@gmail.com>
2021-06-27 01:36:14 -0400

    add integration tests for sqgen-mysql tables

---
+++ sqgen/mysql/mock.go
@@ -0,0 +1,9 @@
+package mysql
+
+var _ Logger = (*mockLogger)(nil)
+
+type mockLogger struct{}
+
+func (l *mockLogger) Printf(f string, v ...interface{}) {}
+
+func (l *mockLogger) Println(v ...interface{}) {}
