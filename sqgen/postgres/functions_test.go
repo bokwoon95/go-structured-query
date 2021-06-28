62eff2f15f896de825e492bb739bbf028db224a8
Quentin Fox <quentinbennettfox@gmail.com>
2021-06-28 00:51:15 -0400

    add tests for extractNameAndType

--- sqgen/postgres/functions_test.go
+++ sqgen/postgres/functions_test.go
@@ -187,3 +187,343 @@ func TestGetFieldName(t *testing.T) {
 		})
 	}
 }
+
+func TestExtractNameAndType(t *testing.T) {
+	type TT struct {
+		name string
+		rawField string
+		result FunctionField
+	}
+
+	tests := []TT{
+		{
+			name: "anon boolean field",
+			rawField: " boolean",
+			result: FunctionField{
+				Name: "",
+				RawField: "boolean",
+				FieldType: FieldTypeBoolean,
+				GoType: GoTypeBool,
+				Constructor: FieldConstructorBoolean,
+			},
+		},
+		{
+			name: "named boolean field",
+			rawField: "flag boolean",
+			result: FunctionField{
+				Name: "flag",
+				RawField: "flag boolean",
+				FieldType: FieldTypeBoolean,
+				GoType: GoTypeBool,
+				Constructor: FieldConstructorBoolean,
+			},
+		},
+		{
+			name: "boolean slice field",
+			rawField: "flags boolean[]",
+			result: FunctionField{
+				Name: "flags",
+				RawField: "flags boolean[]",
+				FieldType: FieldTypeArray,
+				GoType: GoTypeBoolSlice,
+				Constructor: FieldConstructorArray,
+			},
+		},
+		{
+			name: "anon json field",
+			rawField: " json",
+			result: FunctionField{
+				Name: "",
+				RawField: "json",
+				FieldType: FieldTypeJSON,
+				GoType: GoTypeInterface,
+				Constructor: FieldConstructorJSON,
+			},
+		},
+		{
+			name: "named json field",
+			rawField: "data json",
+			result: FunctionField{
+				Name: "data",
+				RawField: "data json",
+				FieldType: FieldTypeJSON,
+				GoType: GoTypeInterface,
+				Constructor: FieldConstructorJSON,
+			},
+		},
+		{
+			name: "anon jsonb field",
+			rawField: " jsonb",
+			result: FunctionField{
+				Name: "",
+				RawField: "jsonb",
+				FieldType: FieldTypeJSON,
+				GoType: GoTypeInterface,
+				Constructor: FieldConstructorJSON,
+			},
+		},
+		{
+			name: "named jsonb field",
+			rawField: "data jsonb",
+			result: FunctionField{
+				Name: "data",
+				RawField: "data jsonb",
+				FieldType: FieldTypeJSON,
+				GoType: GoTypeInterface,
+				Constructor: FieldConstructorJSON,
+			},
+		},
+		{
+			name: "json array field",
+			rawField: "data_arr json[]",
+			result: FunctionField{
+				Name: "data_arr",
+				RawField: "data_arr json[]",
+				FieldType: FieldTypeArray,
+				GoType: GoTypeInterface,
+				Constructor: FieldConstructorArray,
+			},
+		},
+	}
+
+	intFields := []string{
+		"smallint",
+		"oid",
+		"integer",
+		"bigint",
+		"smallserial",
+		"bigserial",
+	}
+
+	for _, rawField := range intFields {
+		tests = append(tests, TT{
+			name: "anon " + rawField + " field",
+			rawField: rawField,
+			result: FunctionField{
+				Name: "",
+				RawField: rawField,
+				FieldType: FieldTypeNumber,
+				GoType: GoTypeInt,
+				Constructor: FieldConstructorNumber,
+			},
+		})
+
+		tests = append(tests, TT{
+			name: "named " + rawField + " field",
+			rawField: "value " + rawField,
+			result: FunctionField{
+				Name: "value",
+				RawField: "value " + rawField,
+				FieldType: FieldTypeNumber,
+				GoType: GoTypeInt,
+				Constructor: FieldConstructorNumber,
+			},
+		})
+
+		tests = append(tests, TT{
+			name: "anon " + rawField + " array field",
+			rawField: rawField + "[]",
+			result: FunctionField{
+				Name: "",
+				RawField: rawField + "[]",
+				FieldType: FieldTypeArray,
+				GoType: GoTypeIntSlice,
+				Constructor: FieldConstructorArray,
+			},
+		})
+	}
+
+	floatFields := []string{
+		"decimal",
+		"numeric",
+		"real",
+		"double precision",
+	}
+
+	for _, rawField := range floatFields {
+		tests = append(tests, TT{
+			name: "anon " + rawField + " field",
+			rawField: rawField,
+			result: FunctionField{
+				Name: "",
+				RawField: rawField,
+				FieldType: FieldTypeNumber,
+				GoType: GoTypeFloat64,
+				Constructor: FieldConstructorNumber,
+			},
+		})
+
+		tests = append(tests, TT{
+			name: "named " + rawField + " field",
+			rawField: "value " + rawField,
+			result: FunctionField{
+				Name: "value",
+				RawField: "value " + rawField,
+				FieldType: FieldTypeNumber,
+				GoType: GoTypeFloat64,
+				Constructor: FieldConstructorNumber,
+			},
+		})
+
+		tests = append(tests, TT{
+			name: "anon " + rawField + " array field",
+			rawField: rawField + "[]",
+			result: FunctionField{
+				Name: "",
+				RawField: rawField + "[]",
+				FieldType: FieldTypeArray,
+				GoType: GoTypeFloat64Slice,
+				Constructor: FieldConstructorArray,
+			},
+		})
+	}
+
+	stringFields := []string{
+		"text",
+		"name",
+		"char",
+		"char(64)",
+		"character",
+		"character(8)",
+		"varchar",
+		"varchar(128)",
+		"character varying",
+		"character varying(256)",
+	}
+
+	for _, rawField := range stringFields {
+		tests = append(tests, TT{
+			name: "anon " + rawField + " field",
+			rawField: rawField,
+			result: FunctionField{
+				Name: "",
+				RawField: rawField,
+				FieldType: FieldTypeString,
+				GoType: GoTypeString,
+				Constructor: FieldConstructorString,
+			},
+		})
+
+		tests = append(tests, TT{
+			name: "named " + rawField + " field",
+			rawField: "first_name " + rawField,
+			result: FunctionField{
+				Name: "first_name",
+				RawField: "first_name " + rawField,
+				FieldType: FieldTypeString,
+				GoType: GoTypeString,
+				Constructor: FieldConstructorString,
+			},
+		})
+
+		tests = append(tests, TT{
+			name: "anon " + rawField + " array field",
+			rawField: rawField + "[]",
+			result: FunctionField{
+				Name: "",
+				RawField: rawField + "[]",
+				FieldType: FieldTypeArray,
+				GoType: GoTypeStringSlice,
+				Constructor: FieldConstructorArray,
+			},
+		})
+	}
+
+
+	timeFields := []string{
+		"date",
+		"time",
+		"timestamp",
+		"date with time zone",
+		"time with time zone",
+		"timestamp with time zone",
+		"date without time zone",
+		"time without time zone",
+		"timestamp without time zone",
+		"date (32)",
+		"time (64)",
+		"timestamp (128)",
+		"date (32) with time zone",
+		"time (64) with time zone",
+		"timestamp (128) with time zone",
+		"date (128) without time zone",
+		"time (64) without time zone",
+		"timestamp (32) without time zone",
+	}
+
+	for _, rawField := range timeFields {
+		tests = append(tests, TT{
+			name: "anon " + rawField + " field",
+			rawField: rawField,
+			result: FunctionField{
+				Name: "",
+				RawField: rawField,
+				FieldType: FieldTypeTime,
+				GoType: GoTypeTime,
+				Constructor: FieldConstructorTime,
+			},
+		})
+
+		tests = append(tests, TT{
+			name: "named " + rawField + " field",
+			rawField: "value " + rawField,
+			result: FunctionField{
+				Name: "value",
+				RawField: "value " + rawField,
+				FieldType: FieldTypeTime,
+				GoType: GoTypeTime,
+				Constructor: FieldConstructorTime,
+			},
+		})
+
+		// no fields added if it's an array of times
+		tests = append(tests, TT{
+			name: "anon " + rawField + " array field (skipped)",
+			rawField: rawField + "[]",
+			result: FunctionField{
+				Name: "",
+				RawField: rawField + "[]",
+			},
+		})
+	}
+
+	tests = append(tests, TT{
+		name: "anon bytea field",
+		rawField: "bytea",
+		result: FunctionField{
+			Name: "",
+			RawField: "bytea",
+			FieldType: FieldTypeBinary,
+			GoType: GoTypeByteSlice,
+			Constructor: FieldConstructorBinary,
+		},
+	})
+
+	tests = append(tests, TT{
+		name: "named bytea field",
+		rawField: "hash bytea",
+		result: FunctionField{
+			Name: "hash",
+			RawField: "hash bytea",
+			FieldType: FieldTypeBinary,
+			GoType: GoTypeByteSlice,
+			Constructor: FieldConstructorBinary,
+		},
+	})
+
+	tests = append(tests, TT{
+		name: "anon bytea array field (skipped)",
+		rawField: "bytea[]",
+		result: FunctionField{
+			Name: "",
+			RawField: "bytea[]",
+		},
+	})
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			is := is.New(t)
+			is.Equal(extractNameAndType(tt.rawField), tt.result)
+		})
+	}
+}
