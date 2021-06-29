a66fa75f8074ef4533385009f27dd6ed1ebe38b8
Quentin Fox <quentinbennettfox@gmail.com>
2021-06-29 14:12:34 -0400

    add test for goimports

---
+++ sqgen/format_test.go
@@ -0,0 +1,30 @@
+package sqgen
+
+import (
+	"testing"
+	
+	"github.com/matryer/is"
+)
+
+func TestFormatOutput(t *testing.T) {
+	is := is.New(t)
+
+	input := `package tables
+
+func Something(column time.Time) bool {
+	return true
+}`
+
+	expected := `package tables
+
+import "time"
+
+func Something(column time.Time) bool {
+	return true
+}
+`
+
+	out, err := FormatOutput([]byte(input))
+	is.NoErr(err)
+	is.Equal(string(out), expected)
+}
